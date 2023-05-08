e17d4cc3065cbcc8e0576796acdf7d12cae4ad4c
DanteLore <dan.taylor@live.co.uk>
2023-05-08 18:17:22 +0100

    Woodcuters are working

---
+++ Assets/Scripts/Woodcutter.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
+public class Woodcutter : MonoBehaviour
+{
+    public WoodcuttersHut hut;
+    public Vector3 spawnPoint;
+    public string TreeLayer = "Nature";
+    public float destinationThreshold = 1.0f;
+    
+    private NavMeshAgent navAgent;
+    private GameObject body;
+    private List<GameObject> blacklist = new List<GameObject>();
+
+    void Start()
+    {
+        Debug.Log("I'm alive!");
+
+        hut = GetComponentInParent<WoodcuttersHut>();
+        spawnPoint = hut.spawnPoint.transform.position;
+        navAgent = GetComponent<NavMeshAgent>();
+        body = transform.Find("Body").gameObject;
+
+        StartCoroutine(WoodcuttersLoop());
+    }
+
+    private IEnumerator WoodcuttersLoop()
+    {
+        yield return new WaitForSeconds(1);
+
+        while(true)
+        {
+            var tree = GetTree(transform.position);
+
+            if (tree == null)
+            {
+                Debug.Log("No trees left!");
+                break;
+            }
+
+            var dest = tree.transform.position;
+            navAgent.SetDestination(dest);
+            navAgent.isStopped = false;
+            yield return null;
+
+            float stillTime = 0.0f;
+            while (!CloseEnoughTo(dest))
+            {
+                stillTime = (navAgent.velocity.magnitude < 0.5f) ? stillTime + Time.deltaTime : 0.0f;
+
+                if(stillTime > 10.0f)
+                {
+                    Debug.Log("I got stuck!  Going to blacklist this tree");
+                    blacklist.Add(tree);
+                    tree = null;
+                    yield return new WaitForSeconds(1);
+                    break;
+                }
+                else
+                {
+                    yield return null;
+                }
+            }
+
+            if(tree)
+            {
+                Debug.Log("Gonna chop this here tree!");
+                navAgent.isStopped = true;
+                yield return new WaitForSeconds(3);
+                Destroy(tree);
+                yield return new WaitForSeconds(2);
+            }
+
+            Debug.Log("Headin' home");
+            dest = spawnPoint;
+            navAgent.SetDestination(dest);
+            navAgent.isStopped = false;
+            yield return null;
+
+            while (!CloseEnoughTo(dest))
+                yield return null;
+
+            Debug.Log("Home again.  Time for a snooze");
+            navAgent.isStopped = true;
+            body.SetActive(false);
+            yield return new WaitForSeconds(5);
+
+            Debug.Log("Back to work!");
+            body.SetActive(true);
+        }
+    }
+
+    private bool CloseEnoughTo(Vector3 dest)
+    {
+        Vector3 v1 = new Vector3(transform.position.x, 0.0f, transform.position.z);
+        Vector3 v2 = new Vector3(dest.x, 0.0f, dest.z);
+
+        return Vector3.SqrMagnitude(v1 - v2) < destinationThreshold;
+    }
+
+    private GameObject GetTree(Vector3 pos)
+    {
+        var candidates = Physics.OverlapSphere(pos, 500, LayerMask.GetMask(TreeLayer));
+
+        return candidates
+            .Select(c => c.gameObject)
+            .Except(blacklist)
+            .Where(go => go.GetComponent<Tree>() != null)
+            .OrderBy(go => Vector3.SqrMagnitude(go.transform.position - pos))
+            .FirstOrDefault();
+    }
+}
--- Assets/Scripts/WoodcuttersHut.cs
+++ Assets/Scripts/WoodcuttersHut.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,22 @@ using UnityEngine;
 public class WoodcuttersHut : MonoBehaviour
 {
     public GameObject spawnPoint;
+
+    public GameObject woodcutterPrefab;
+
+    public float spawnDelaySeconds = 10f;
+
+    private GameObject woodcutter;
+
+    void Start()
+    {
+        StartCoroutine(CreateWoodcutter(spawnDelaySeconds));
+    }
+
+    private IEnumerator CreateWoodcutter(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        woodcutter = Instantiate(woodcutterPrefab, spawnPoint.transform.position, Quaternion.identity, transform);
+    }
 }
