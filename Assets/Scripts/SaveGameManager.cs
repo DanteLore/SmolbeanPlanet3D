d111bbdaa8506a5182ad38ec8edbd04e76e09f24
DanteLore <dan.taylor@live.co.uk>
2023-05-13 18:57:12 +0100

    Quick hack of saving the game map... and it actually worked

--- Assets/Scripts/SaveGameManager.cs
+++ Assets/Scripts/SaveGameManager.cs
@@ -3,6 +3,7 @@ using System.IO;
 using UnityEngine;
 using System.Linq;
 using System;
+using Newtonsoft.Json;  
 
 public class SaveGameManager : MonoBehaviour
 {
@@ -26,7 +27,18 @@ public class SaveGameManager : MonoBehaviour
         if(File.Exists(filename))
             File.Delete(filename);
 
-        File.Create(filename).Close(); // Literally just an empty file for now :)
+        // Things to be saved:
+        // 1. The game map (which allows regeneration of the island mesh)
+        // 2. The locations, rotations and scale of all the trees
+        // 3. The locations, rotations and scale of all the buildings
+        // 4. The location and state of all the creatures etc - future.
+        // 5. Likewise items (wood, stone etc), stats, history etc etc
+
+        using (StreamWriter file = File.CreateText(filename))
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Serialize(file, GameObject.FindAnyObjectByType<GridManager>().GameMap);
+        }
     }
 
     private static string GetFilename(string name)
@@ -52,7 +64,15 @@ public class SaveGameManager : MonoBehaviour
     internal static void LoadGame(string name)
     {
         string filename = GetFilename(name);
-        
         Debug.Log($"Loading game from {filename}");
+
+        using (StreamReader file = File.OpenText(filename))
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            List<int> map = (List<int>)serializer.Deserialize(file, typeof(List<int>));
+            GameObject.FindAnyObjectByType<GridManager>().Recreate(map, 100, 100);
+            MenuController.Instance.CloseAll();
+        }
+
     }
 }
