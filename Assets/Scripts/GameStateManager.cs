e9de1e8716dc5ecbf6528c778e81ae476896e00f
DanteLore <dan.taylor@live.co.uk>
2023-12-18 18:48:23 +0000

    Coming back to the project after a long break, an unfinished dodo and adjustable game speed added

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -6,8 +6,20 @@ public class GameStateManager : MonoBehaviour
     public static GameStateManager Instance { get; private set; }
     public bool IsPaused { get; private set; }
     public bool IsStarted { get; private set;}
+    public event EventHandler<float> GameSpeedChanged;
     public event EventHandler<bool> GamePauseStateChanged;
     public event EventHandler<bool> GameStarted;
+    private float selectedTimeScale = 1.0f;
+    public float SelectedTimeScale 
+    {
+        get { return selectedTimeScale; }
+        private set
+        {
+            selectedTimeScale = value;
+            Debug.Log("Time scale set to: " + selectedTimeScale);
+            GameSpeedChanged?.Invoke(this, selectedTimeScale);
+        }
+    }
 
     void Awake()
     {
@@ -23,7 +35,7 @@ public class GameStateManager : MonoBehaviour
     public void Pause()
     {
         IsPaused = true;
-        Time.timeScale = 0;
+        Time.timeScale = 0.0f;
         AudioListener.pause = true;
         GamePauseStateChanged?.Invoke(this, IsPaused);
     }
@@ -31,7 +43,7 @@ public class GameStateManager : MonoBehaviour
     public void Resume()
     {
         IsPaused = false;
-        Time.timeScale = 1;
+        Time.timeScale = SelectedTimeScale;
         AudioListener.pause = false;
         GamePauseStateChanged?.Invoke(this, IsPaused);
     }
@@ -40,5 +52,23 @@ public class GameStateManager : MonoBehaviour
     {
         IsStarted = true;
         GameStarted?.Invoke(this, IsStarted);
+    }
+
+    public void Update()
+    {
+        if(IsPaused)
+            return;
+
+        if(Input.GetKeyDown("1"))
+            SelectedTimeScale = 1.0f;
+        else if(Input.GetKeyDown("2"))
+            SelectedTimeScale = 2.0f;
+        else if(Input.GetKeyDown("3"))
+            SelectedTimeScale = 4.0f;
+        else if(Input.GetKeyDown("4"))
+            SelectedTimeScale = 8.0f;
+
+        if(SelectedTimeScale != Time.timeScale)
+            Time.timeScale = SelectedTimeScale;
     }
 }
