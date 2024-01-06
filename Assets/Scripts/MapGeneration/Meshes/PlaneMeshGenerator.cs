462474a8c55835115617287ea2496d48f5ee0a9f
DanteLore <dan.taylor@live.co.uk>
2024-01-06 15:01:21 +0000

    tweaks to clouds, including modifying the real mesh into a bowl shape, rather than using a shader to do it

--- Assets/Scripts/MapGeneration/Meshes/PlaneMeshGenerator.cs
+++ Assets/Scripts/MapGeneration/Meshes/PlaneMeshGenerator.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Linq;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class PlaneMeshGenerator : MonoBehaviour
@@ -12,40 +13,50 @@ public class PlaneMeshGenerator : MonoBehaviour
 
     public bool makeCircularMesh = false;
 
+    public float bowlHeight = 0;
+
     public void GeneratePlane()
     {
         // Create new vertices
         Vector3[] vertices = new Vector3[numVertices * numVertices];
         Vector2[] uv = new Vector2[numVertices * numVertices];
+        Vector3[] normals = new Vector3[numVertices * numVertices];
+
         float unitSize = widthHeight / (numVertices - 1);
         float radius = widthHeight / 2;
         float offset = radius * -1;
         Vector3 meshCenter = Vector3.zero;
-        
-        for (int y = 0; y < numVertices; y++)
+        Vector2 flatMeshCenter = new Vector2(meshCenter.x, meshCenter.z);
+
+        for (int z = 0; z < numVertices; z++)
         {
             for (int x = 0; x < numVertices; x++)
             {
-                vertices[y * numVertices + x] = new Vector3(offset + x * unitSize, 0f, offset + y * unitSize);
-                uv[y * numVertices + x] = new Vector2(x / widthHeight, y / widthHeight);
+                var point = new Vector3(offset + x * unitSize, 0f, offset + z * unitSize);
+                float d = Vector3.Distance(point, meshCenter) / radius;
+                float p = Mathf.Pow(d, 3);
+                point.y -= p * bowlHeight;
+                
+                vertices[z * numVertices + x] = point;
+                uv[z * numVertices + x] = new Vector2(x / widthHeight, z / widthHeight);
             }
         }
 
         // Create new triangles
-        var triangles = new List<int>((numVertices - 1) * (numVertices - 1) * 6);
+        var triangles = new List<int>();
 
-        for (int y = 0; y < numVertices - 1; y++)
+        for (int z = 0; z < numVertices - 1; z++)
         {
             for (int x = 0; x < numVertices - 1; x++)
             {
-                int bottomLeftIndex = y * numVertices + x;
-                int bottomRightIndex = y * numVertices + x + 1;
-                int topLeftIndex = (y + 1) * numVertices + x;
-                int topRightIndex = (y + 1) * numVertices + x + 1;
+                int bottomLeftIndex = z * numVertices + x;
+                int bottomRightIndex = z * numVertices + x + 1;
+                int topLeftIndex = (z + 1) * numVertices + x;
+                int topRightIndex = (z + 1) * numVertices + x + 1;
 
                 var center = (vertices[bottomLeftIndex] + vertices[bottomLeftIndex] + vertices[topLeftIndex] + vertices[bottomRightIndex]) / 4f;
 
-                if(!makeCircularMesh || Vector3.Distance(center, meshCenter) < radius)
+                if(!makeCircularMesh || Vector2.Distance(new Vector2(center.x, center.z), flatMeshCenter) < radius)
                 {
                     triangles.Add(bottomLeftIndex);
                     triangles.Add(topLeftIndex);
@@ -59,12 +70,30 @@ public class PlaneMeshGenerator : MonoBehaviour
         }
 
         // Setup normals
-        Vector3[] normals = new Vector3[numVertices * numVertices];
-        for (int i = 0; i < numVertices * numVertices; i++)
+        for (int i = 0; i < triangles.Count; i += 3)
         {
-            normals[i] = Vector3.up;
+            int va = triangles[i];
+            int vb = triangles[i + 1];
+            int vc = triangles[i + 2];
+
+            Vector3 pointA = vertices[va];
+            Vector3 pointB = vertices[vb];
+            Vector3 pointC = vertices[vc];
+
+            Vector3 sideAB = pointB - pointA;
+            Vector3 sideAC = pointC - pointA;
+
+            var triangleNormal = Vector3.Cross(sideAB, sideAC).normalized;
+
+            normals[va] += triangleNormal;
+            normals[vb] += triangleNormal;
+            normals[vc] += triangleNormal;
         }
 
+        for(int i = 0; i < normals.Length; i++)
+            normals[i].Normalize();
+
+        // Create the mesh
         var mesh = new Mesh
         {
             vertices = vertices,
