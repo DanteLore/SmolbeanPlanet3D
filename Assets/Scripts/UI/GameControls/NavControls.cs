e9b2bccb91d4c90f7e39aff8492972a38af55028
DanteLore <dan.taylor@live.co.uk>
2024-01-08 22:35:22 +0000

    Compass direction display and yet more font sorting

--- Assets/Scripts/UI/GameControls/NavControls.cs
+++ Assets/Scripts/UI/GameControls/NavControls.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,8 +8,10 @@ public class NavigationControls : MonoBehaviour
     public GridManager gridManager;
     public string groundLayer = "Ground";
     public string seaLayer = "Sea";
+    public float rotationOffset = -48f;
 
     private Label positionLabel;
+    private Label directionLabel;
 
     private int groundLayerMask;
     
@@ -18,20 +21,28 @@ public class NavigationControls : MonoBehaviour
         
         groundLayerMask = LayerMask.GetMask(groundLayer, seaLayer);
         positionLabel = document.rootVisualElement.Q<Label>("positionLabel");
+        directionLabel = document.rootVisualElement.Q<Label>("directionLabel");
     }
 
     void Update()
     {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out var hit, float.PositiveInfinity, groundLayerMask))
-            {
-                var pos = gridManager.GetGameSquareFromWorldCoords(hit.point);
-                positionLabel.text = $"{pos.x}λ \u00d7 {pos.y}φ";
-            }
-            else
-            {
-                positionLabel.text = "";
-            }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit, float.PositiveInfinity, groundLayerMask))
+        {
+            var pos = gridManager.GetGameSquareFromWorldCoords(hit.point);
+            positionLabel.text = $"{pos.x}λ \u00d7 {pos.y}φ";
+        }
+        else
+        {
+            positionLabel.text = "";
+        }
 
+        var cameraDirXZ = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up);
+        Vector3.Normalize(cameraDirXZ);
+        float heading = Vector3.SignedAngle(Vector3.forward, cameraDirXZ, Vector3.up);
+        heading += rotationOffset;
+        heading %= 360f;
+        
+        directionLabel.style.rotate = new StyleRotate(new Rotate(new Angle(heading)));
     }
 }
