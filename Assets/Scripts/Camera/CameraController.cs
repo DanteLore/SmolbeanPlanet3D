b90fb70f0eecdb50d15ae30216ab9bc7e24e6da5
DanteLore <dan.taylor@live.co.uk>
2024-04-24 11:00:42 +0100

    Moved camera control over to new input system

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,21 +1,22 @@
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 // https://www.youtube.com/watch?v=pJQndtJ2rk0
+// https://www.youtube.com/watch?v=ZSP3bFaZm-o
 public class CameraController : MonoBehaviour
 {    
-    public CinemachineVirtualCamera virtualCamera;
-    public float speedMin = 30f;
-    public float speedMax = 300f;
-    public float rotateSpeedMin = 90f;
-    public float rotateSpeedMax = 180f;
-    public float mouseRotationSpeedMax = 2f;
-    public float zoomStep = 10f;
-    public float zoomSpeed = 5f;
-    public float zoomDistanceMin = 10f;
-    public float zoomDistanceMax = 500f;
-    public Vector3 lookAngleMin = new(0f, 0.1f, 0.8f);
-    public Vector3 lookAngleMax = new(0f, 0.8f, -0.1f);
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private float speedMin = 30f;
+    [SerializeField] private float speedMax = 300f;
+    [SerializeField] private float rotateSpeedMin = 90f;
+    [SerializeField] private float rotateSpeedMax = 180f;
+    [SerializeField] private float zoomStep = 10f;
+    [SerializeField] private float zoomSpeed = 5f;
+    [SerializeField] private float zoomDistanceMin = 10f;
+    [SerializeField] private float zoomDistanceMax = 500f;
+    [SerializeField] private Vector3 lookAngleMin = new(0f, 0.1f, 0.8f);
+    [SerializeField] private Vector3 lookAngleMax = new(0f, 0.8f, -0.1f);
 
     private GridManager gridManager;
     private CinemachineTransposer transposer;
@@ -23,6 +24,19 @@ public class CameraController : MonoBehaviour
     private float targetZoom;
     private float speed;
     private float rotateSpeed;
+    private SmolbeanInputActions actions;
+    private InputAction movementAction;
+    private InputAction rotationAction;
+    private float rotationInput;
+    private float zoomInput;
+
+    void Awake()
+    {
+        actions = new SmolbeanInputActions();
+        movementAction = actions.GodView.Movement;
+        rotationAction = actions.GodView.RotateCamera;
+        actions.GodView.ZoomCamera.performed += CameraZoomInput;
+    }
 
     void Start()
     {
@@ -30,6 +44,18 @@ public class CameraController : MonoBehaviour
         transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
         zoomDistance = transposer.m_FollowOffset.magnitude;
         targetZoom = zoomDistance;
+
+        actions.GodView.Enable();
+    }
+
+    private void CameraRotationInput(InputAction.CallbackContext context)
+    {
+        rotationInput = context.ReadValue<Vector2>().x;
+    }
+
+    private void CameraZoomInput(InputAction.CallbackContext context)
+    {
+        zoomInput = context.ReadValue<Vector2>().y / 100f;
     }
 
     void Update()
@@ -37,22 +63,27 @@ public class CameraController : MonoBehaviour
         if (GameStateManager.Instance.IsPaused)
             return;
 
-        Vector3 input = GetMovementInput();
-        speed = input.magnitude < 1.0f ? speedMin : Mathf.Lerp(speed, speedMax, Time.unscaledDeltaTime);
-        Vector3 move = transform.forward * input.z + transform.right * input.x;
-        move *= speed * Time.unscaledDeltaTime;
-        var newPos = transform.position + move;
-        float y = gridManager.GetMaxGridHightAt(newPos.x, newPos.z);
-        y = float.IsNaN(y) ? 1f : y;
-        y = Mathf.Max(y, 1f);
-        transform.position = new Vector3(newPos.x, y, newPos.z);
+        if(movementAction.inProgress)
+        {
+            Vector2 input = movementAction.ReadValue<Vector2>();
+            speed = input.magnitude < 1.0f ? speedMin : Mathf.Lerp(speed, speedMax, Time.unscaledDeltaTime);
+            Vector3 move = transform.forward * input.y + transform.right * input.x;
+            move *= speed * Time.unscaledDeltaTime;
+            var newPos = transform.position + move;
+            float y = gridManager.GetMaxGridHightAt(newPos.x, newPos.z);
+            y = float.IsNaN(y) ? 1f : y;
+            y = Mathf.Max(y, 1f);
+            transform.position = new Vector3(newPos.x, y, newPos.z);
+        }
 
-        float rotation = GetRotationInput();
-        rotateSpeed = Mathf.Abs(rotation) < 1 ? rotateSpeedMin : Mathf.Lerp(rotateSpeed, rotateSpeedMax, Time.unscaledDeltaTime);
-        rotation *= rotateSpeed * Time.unscaledDeltaTime;
-        transform.eulerAngles += new Vector3(0f, rotation, 0f);
-
-        float zoomInput = Input.mouseScrollDelta.y;
+        if(rotationAction.activeControl != null && 
+            (!rotationAction.activeControl.path.Contains("Mouse") || Mouse.current.rightButton.isPressed))
+        {
+            rotationInput = rotationAction.ReadValue<Vector2>().x;
+            rotateSpeed = Mathf.Abs(rotationInput) < 1 ? rotateSpeedMin : Mathf.Lerp(rotateSpeed, rotateSpeedMax, Time.unscaledDeltaTime);
+            rotationInput *= rotateSpeed * Time.unscaledDeltaTime;
+            transform.eulerAngles += new Vector3(0f, rotationInput, 0f);
+        }
 
         if(zoomInput > 0f)
             targetZoom -= zoomStep;
@@ -66,30 +97,8 @@ public class CameraController : MonoBehaviour
         
         zoomDistance = Mathf.Lerp(zoomDistance, targetZoom, Time.unscaledDeltaTime * zoomSpeed);
         transposer.m_FollowOffset = lookVector * zoomDistance;
-    }
 
-    private static float GetRotationInput()
-    {
-        float rotation = 0f;
-        if (Input.GetKey(KeyCode.Q))
-            rotation -= 1f;
-        if (Input.GetKey(KeyCode.E))
-            rotation += 1f;
-        return rotation;
-    }
-
-    private static Vector3 GetMovementInput()
-    {
-        Vector3 input = new(0f, 0f, 0f);
-
-        if (Input.GetKey(KeyCode.W))
-            input.z += 1.0f;
-        if (Input.GetKey(KeyCode.A))
-            input.x -= 1.0f;
-        if (Input.GetKey(KeyCode.S))
-            input.z -= 1.0f;
-        if (Input.GetKey(KeyCode.D))
-            input.x += 1.0f;
-        return input;
+        // Reset inputs we've digested
+        zoomInput = 0f;
     }
 }
