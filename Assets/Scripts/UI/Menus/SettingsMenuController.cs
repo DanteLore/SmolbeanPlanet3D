f85445be10cdb414be6c06e576ba072dba099147
DanteLore <dan.taylor@live.co.uk>
2025-06-04 20:32:39 +0100

    Added graphics quality settings to the settings menu - makes a massive difference to performance

--- Assets/Scripts/UI/Menus/SettingsMenuController.cs
+++ Assets/Scripts/UI/Menus/SettingsMenuController.cs
@@ -2,6 +2,7 @@ using UnityEngine.UIElements;
 using UnityEngine.Audio;
 using UnityEngine;
 using System.Linq;
+using System.Collections.Generic;
 
 public class SettingsMenuController : SmolbeanMenu
 {
@@ -12,66 +13,82 @@ public class SettingsMenuController : SmolbeanMenu
     void OnEnable()
     {
         document = GetComponent<UIDocument>();
+        var root = document.rootVisualElement;
         soundPlayer = GameObject.Find("SFXManager").GetComponent<SoundPlayer>();
 
-        var doneButton = document.rootVisualElement.Q<Button>("doneButton");
+        var doneButton = root.Q<Button>("doneButton");
         doneButton.clicked += DoneButtonClicked;
 
-        var musicVolumeSlider = document.rootVisualElement.Q<Slider>("musicSlider");
+        var musicVolumeSlider = root.Q<Slider>("musicSlider");
         musicVolumeSlider.value = PrefsManager.Instance.MusicVolume;
         musicVolumeSlider.RegisterValueChangedCallback(v =>
         {
             PrefsManager.Instance.MusicVolume = v.newValue;
         });
 
-        var sfxVolumeSlider = document.rootVisualElement.Q<Slider>("sfxSlider");
+        var sfxVolumeSlider = root.Q<Slider>("sfxSlider");
         sfxVolumeSlider.value = PrefsManager.Instance.SfxVolume;
         sfxVolumeSlider.RegisterValueChangedCallback(v =>
         {
             PrefsManager.Instance.SfxVolume = v.newValue;
         });
 
-        var ambientVolumeSlider = document.rootVisualElement.Q<Slider>("ambientSlider");
+        var ambientVolumeSlider = root.Q<Slider>("ambientSlider");
         ambientVolumeSlider.value = PrefsManager.Instance.AmbientVolume;
         ambientVolumeSlider.RegisterValueChangedCallback(v =>
         {
             PrefsManager.Instance.AmbientVolume = v.newValue;
         });
 
-        var grassToggle = document.rootVisualElement.Q<Toggle>("grassToggle");
+        var grassToggle = root.Q<Toggle>("grassToggle");
         grassToggle.value = PrefsManager.Instance.GrassRenderingEnabled;
-        grassToggle.RegisterValueChangedCallback(v => 
+        grassToggle.RegisterValueChangedCallback(v =>
         {
             PrefsManager.Instance.GrassRenderingEnabled = v.newValue;
         });
 
-        var cloudsToggle = document.rootVisualElement.Q<Toggle>("cloudsToggle");
+        var cloudsToggle = root.Q<Toggle>("cloudsToggle");
         cloudsToggle.value = PrefsManager.Instance.CloudsEnabled;
-        cloudsToggle.RegisterValueChangedCallback(v => 
+        cloudsToggle.RegisterValueChangedCallback(v =>
         {
             PrefsManager.Instance.CloudsEnabled = v.newValue;
         });
 
-        var fullscreenToggle = document.rootVisualElement.Q<Toggle>("fullscreenToggle");
+        var fullscreenToggle = root.Q<Toggle>("fullscreenToggle");
         fullscreenToggle.value = Screen.fullScreen;
-        cloudsToggle.RegisterValueChangedCallback(v => 
+        cloudsToggle.RegisterValueChangedCallback(v =>
         {
             Resolution res = v.newValue ? Screen.resolutions.Last() : Screen.resolutions.First(r => r.width == 1024);
             Screen.SetResolution(res.width, res.height, v.newValue ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed);
         });
 
-        var resolutionDropdown = document.rootVisualElement.Q<DropdownField>("resolutionDropdown");
+        var resolutionDropdown = root.Q<DropdownField>("resolutionDropdown");
         var resolutions = Screen.resolutions.Select(r => $"{r.width}x{r.height}").ToList();
         var currentResolutionIndex = Screen.resolutions.ToList().IndexOf(Screen.currentResolution);
         resolutionDropdown.choices = resolutions;
         resolutionDropdown.index = currentResolutionIndex;
-        resolutionDropdown.RegisterValueChangedCallback(v => 
+        resolutionDropdown.RegisterValueChangedCallback(v =>
         {
             var newRes = Screen.resolutions[resolutionDropdown.choices.IndexOf(v.newValue)];
             Screen.SetResolution(newRes.width, newRes.height, Screen.fullScreenMode);
 
             fullscreenToggle.value = Screen.fullScreen;
         });
+
+        var qualityDropdown = root.Q<DropdownField>("qualityDropdown");
+        var qualityLevels = new List<string>(QualitySettings.names);
+        qualityDropdown.choices = qualityLevels;
+        var currentQuality = QualitySettings.GetQualityLevel();
+        qualityDropdown.value = qualityLevels[currentQuality];
+        qualityDropdown.RegisterValueChangedCallback(v =>
+        {
+            int newLevel = qualityLevels.IndexOf(v.newValue);
+            if (newLevel >= 0)
+            {
+                QualitySettings.SetQualityLevel(newLevel);
+            }
+        });
+
     }
 
     private void DoneButtonClicked()
